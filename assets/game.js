7df318afcbb8cb529184fc0730b6b2dc1c171d16
KTurnage <kristaturnage314@gmail.com>
2019-03-06 17:12:59 -0700

    added bootstrap css

--- assets/game.js
+++ assets/game.js
@@ -1,61 +1,58 @@
-var targetNumber = 5; //target number, randomly generated
-var randomCrystalValue; //random number assigned to crystals
+var targetNumber = 0; //target number, randomly generated
+var crystalArray = [{ crystal: "assets/images/black-crystal.jpeg", score: 0 }, { crystal: "assets/images/blue-crystal.jpeg", score: 0 }, { crystal: "assets/images/purple-crystal.jpeg", score: 0 }, { crystal: "assets/images/white-crystal.jpeg", score: 0 }]; //random number assigned to crystals
 var counter = 0;
 var wins = 0;
 var losses = 0;
-var button;
 
 // -- Crystals need to be displayed as buttons -- //
 // onclick event to add value of each crystal to the score//
+function startGame() {
+    for (var i = 0; i < crystalArray.length; i++) {
+        var crystalImage = $("<img>");
+        crystalImage.addClass("crystal-image")
+        crystalImage.attr("src", crystalArray[i].crystal)
+        crystalArray[i].score = Math.floor(Math.random() * 12) + 1
+        crystalImage.attr("data-randomNum", crystalArray[i].score);
+        $("#crystals").append(crystalImage)
+    }
+    targetNumber = Math.floor(Math.random() * (102)) + 19;
+    $("#number-to-guess").text(targetNumber);
+}
 
-// crystal1 button needs randomCrystalValue;
-// crystal2 button needs randomCrystalValue;
-// crystal3 button needs randomCrystalValue;
-// crystal4 button needs randomCrystalValue;
+//Reset Function
+function resetGame() {
+    targetNumber = 0;
+    counter = 0;
+    $("#number-to-guess, #crystals, #current-score").text("");
+}
 
-//if the players score is === targetNumber the player WINS..add to win count
-//if the players score is < targetNumber the player continues to play..
-//else the player score is > targetNumber the player LOOSES..add to loss count
+startGame();
 
-//set the "number-to-guess" header to match targetNumber
-$("#number-to-guess").text(targetNumber);
-//set the "current-score" neader to match counter
-$("#current-score").text(counter);
-//set "wins" header to match wins
-$("#wins").text(wins);
-//set "losses header to match losses"
-$("#losses").text(losses);
-
-
-//?? can I add an array of the 4 crystal images here?
-$(".crystal-image1").on("click", function () {
-    counter += 1;
-    alert("Your new score is: " + counter); //change from an alert to update counter
-
-
+$("#crystals").on("click", ".crystal-image", function () {
+    counter += parseInt($(this).attr("data-randomNum"));
+    $("#current-score").text(counter);
 
     if (counter === targetNumber) {
         alert("WINNER WINNER CHICKEN DINNER!");
-    } ifelse, { 
-        (counter < targetNumber) {
-        //something happens here
-    }else {
-        (counter > targetNumber) {
-            alert("YOU LOSE!")
-        })
-        }
+        wins++
+        $("#wins").text(wins);
+        resetGame ();
+        startGame ();
+    } else if (counter > targetNumber) {
+        alert("YOU LOSE!")
+        losses++
+        $("#losses").text(losses);
+        resetGame ();
+        startGame ();
 
     }
-    
-});
+
+
+})
 
 
 
-// -- onclick at start of the game -- //
-//generate a targetNumber as goal number to reach
-//game restarts when the player wins or loses
-//..with reset targetNumber, hidden values on crystals, score set to 0, score counter set to 0
 
-$(".crystal-image").on("click", function () {
-    alert("You clicked a crystal!")
-});
+
+
+
